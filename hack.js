606c592c4493db5e8e8fbb8507296682fc4cd482
chetan shende <chetanshende137@gmail.com>
2023-09-28 19:32:20 +0530

    new files

---
+++ hack.js
@@ -0,0 +1,12 @@
+function login(){
+    var number = document.getElementById("number").value;
+    if(number == 9022729825){
+        alert("succrss full !..")
+        window.location.assign("newhack.html")
+    }else{
+alert('plase enter a valid number.');
+
+
+
+    }
+}
