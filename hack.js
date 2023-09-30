b73562925e0776cf3323e79ad723e27400bf69f5
chetan shende <chetanshende137@gmail.com>
2023-10-01 00:28:28 +0530

    add imges

--- hack.js
+++ hack.js
@@ -1,12 +1,108 @@
 function login(){
     var number = document.getElementById("number").value;
-    if(number == 9022729825){
-        alert("succrss full !..")
+    if(number == '9022729825'){
+        alert("success full !..")
         window.location.assign("newhack.html")
     }else{
-alert('plase enter a valid number.');
+alert('plase enter a valid number...');
 
 
 
     }
 }
+function adfile(){
+    const file = document.getElementById('btn').value;
+        window.location.assign("newhack2.html")
+    if(file.onclick){
+                window.location.assign("newhack2.html")
+    }
+else{
+        alert("some error plzz try again later..")
+}
+
+
+ }
+function adfile1(){
+    const file1 = document.getElementById('btn1').value;
+window.location.assign("about.html")
+    if(file1.onclick){
+                window.location.assign("about.html")
+    }
+else{
+        alert("some error plzz try again later..")
+}
+}
+
+function adfile2(){
+    const file2 = document.getElementById('btn2').value;
+        window.location.assign("about.html")
+    if(file2.onclick){
+                window.location.assign("exhi.html")
+    }
+else{
+        alert("some error plzz try again later..")
+}
+}
+
+function adfile3(){
+    const file3 = document.getElementById('btn3').value;
+        window.location.assign("event.html")
+    if(file3.onclick){
+                window.location.assign("event.html")
+    }
+else{
+        alert("some error plzz try again later..")
+}
+}
+
+function adfile4(){
+    const file4 = document.getElementById('btn4').value;
+        window.location.assign("store.html")
+    if(file4.onclick){
+                window.location.assign("store.html")
+    }
+else{
+        alert("some error plzz try again later..")
+}
+}
+
+function adfile5(){
+    const file5 = document.getElementById('btn5').value;
+        window.location.assign("contact.html")
+    if(file5.onclick){
+                window.location.assign("contact.html")
+    }
+else{
+        alert("some error plzz try again later..")
+}
+}
+
+
+function adfile6(){
+    const file6= document.getElementById('btn6').value;
+        window.location.assign("feedback.html")
+    if(file6.onclick){
+                window.location.assign("feedback.html")
+    }
+else{
+        alert("some error plzz try again later..")
+}
+}
+
+function skill(){
+    const skill = document.getElementById("skill").value;
+     window.location.assign("apskills.html")
+
+
+
+
+    }
+
+    function center(){
+    const skill = document.getElementById("center").value;
+     window.location.assign("center.html")
+
+
+
+
+    }
